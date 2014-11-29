b1f7e09dd835d84f4dbee5bce9501edb847ad48f
cuterxy <cuterxy@163.com>
2014-11-29 19:52:21 +0800

    customize the controller and mail templates of devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Users::SessionsController < Devise::SessionsController
+# before_filter :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    respond_to_on_destroy
+  end
+
+  # protected
+
+  # You can put the params you want to permit in the empty array.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+end
