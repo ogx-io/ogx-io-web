e071c074e9a0ec1b97b48e4d7032bee39922994d
cuterxy <cuterxy@163.com>
2014-12-31 12:46:20 +0800

    fix a bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized(exception)
-    flash[:error] = exception.policy.err_msg || '您没有此操作的权限！'
+    message = exception.policy.err_msg || '您没有此操作的权限！'
 
     respond_to do |format|
-      format.html { redirect_to(request.referrer || root_path) }
-      format.js { render js: "alert('#{flash[:error]}');" }
+      format.html { flash[:error] = message; redirect_to(request.referrer || root_path) }
+      format.js { render js: "alert('#{message}');" }
     end
 
   end
