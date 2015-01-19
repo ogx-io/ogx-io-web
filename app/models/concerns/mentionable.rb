d57fe08e2d3e33e5df720adf75e8d5bfdbc9f7d4
cuterxy <cuterxy@163.com>
2015-01-19 12:34:19 +0800

    fix a bug

--- app/models/concerns/mentionable.rb
+++ app/models/concerns/mentionable.rb
@@ -2,18 +2,24 @@ module Mentionable
   extend ActiveSupport::Concern
 
   included do
+    field :mentioned_user_ids, type: Array, default: []
     has_many :notification_mentions, as: :mentionable, class_name: 'Notification::Mention'
 
-    after_create :send_mention_notifications
+    after_save :send_mention_notifications
   end
 
   def send_mention_notifications
     names = self.body.scan(/@([a-z0-9_]{4,20})/).flatten
     if names.any?
       user_ids = User.where(:name.in => names).limit(5).collect { |u| u.id }
-      user_ids.each do |uid|
+      uids = user_ids - self.mentioned_user_ids
+      uids.each do |uid|
         Notification::Mention.create(user_id: uid, mentionable: self)
       end
+
+      self.class.skip_callback(:save, :after, :send_mention_notifications)
+      update_attribute(:mentioned_user_ids, self.mentioned_user_ids + uids)
+      self.class.set_callback(:save, :after, :send_mention_notifications)
     end
   end
 
