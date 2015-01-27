ccef836b4ca171995019036a88de01eef7a4c983
cuterxy <cuterxy@163.com>
2015-01-27 23:36:39 +0800

    fix a bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
-NonStupidDigestAssets.whitelist += [/emoji\/.*/]
+NonStupidDigestAssets.whitelist += [/emojis\/.*/]
