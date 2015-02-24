0e42be0b81de4fb63febab83f9de7974264e7438
cuterxy <cuterxy@163.com>
2015-02-24 16:50:30 +0800

    add comment policy rspec code

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -8,20 +8,12 @@ class CommentPolicy < ApplicationPolicy
     signed_in? && (is_supervisor || record.user == user || user.admin?)
   end
 
-  def delete_all?
-    is_supervisor = false
-    if record.commentable_type == 'Post' && record.commentable.board.has_moderator?(user)
-      is_supervisor = true
-    end
-    signed_in? && (is_supervisor || user.admin?)
-  end
-
   def resume?
     is_supervisor = false
     if record.commentable_type == 'Post' && record.commentable.board.has_moderator?(user)
       is_supervisor = true
     end
-    signed_in? && ((is_supervisor && record.deleted == 2) || (user == record.user && record.deleted == 1) || (user.admin? && record.deleted == 3))
+    signed_in? && ((is_supervisor && record.deleted == 2) || (user == record.user && record.deleted == 1) || (user.admin? && record.deleted == 2))
   end
 
   class Scope < Scope
--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -2,27 +2,45 @@ require 'spec_helper'
 
 describe CommentPolicy do
 
-  let(:user) { User.new }
+  let(:user) { create(:user) }
+  let(:author) { create(:user) }
+  let(:admin) { create(:user, :admin) }
+  let(:board) { create(:board) }
+  let(:moderator) { create(:moderator) }
+  let(:post) { create(:post, author: author, board: board) }
+  let(:comment) { create(:comment, user: author, commentable: post) }
 
   subject { CommentPolicy }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  before(:each) { post.board.moderators << moderator }
 
   permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'allows moderator and admin and author to delete comment' do
+      expect(subject).not_to permit(nil, comment)
+      expect(subject).not_to permit(user, comment)
+      expect(subject).to permit(author, comment)
+      expect(subject).to permit(moderator, comment)
+      expect(subject).to permit(admin, comment)
+    end
+  end
+
+  permissions :resume? do
+    it 'allow moderator and admin to resume the comment deleted by them' do
+      comment.delete_by(moderator)
+      expect(subject).not_to permit(nil, comment)
+      expect(subject).not_to permit(user, comment)
+      expect(subject).not_to permit(author, comment)
+      expect(subject).to permit(moderator, comment)
+      expect(subject).to permit(admin, comment)
+    end
+
+    it 'allow author to resume the comment deleted by himself' do
+      comment.delete_by(author)
+      expect(subject).not_to permit(nil, comment)
+      expect(subject).not_to permit(user, comment)
+      expect(subject).to permit(author, comment)
+      expect(subject).not_to permit(moderator, comment)
+      expect(subject).not_to permit(admin, comment)
+    end
   end
 end
