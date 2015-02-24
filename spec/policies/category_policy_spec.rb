6ac4e6717ee9e53d4d3fa598a597d14fe0e541cb
cuterxy <cuterxy@163.com>
2015-02-24 17:10:48 +0800

    add category policy rspec code

--- spec/policies/category_policy_spec.rb
+++ spec/policies/category_policy_spec.rb
@@ -2,27 +2,25 @@ require 'spec_helper'
 
 describe CategoryPolicy do
 
-  let(:user) { User.new }
+  let(:user) { create(:user) }
+  let(:admin) { create(:user, :admin) }
+  let(:category) { create(:category) }
+  let(:new_category) { build(:category) }
 
   subject { CategoryPolicy }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'allows creating category by admin' do
+      expect(subject).not_to permit(user, new_category)
+      expect(subject).to permit(admin, new_category)
+    end
   end
 
   permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'allows updating category by admin' do
+      expect(subject).not_to permit(user, category)
+      expect(subject).to permit(admin, category)
+    end
   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
 end
