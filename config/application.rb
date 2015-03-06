4bac01d105af40515d67cee138c89567dc18a95d
cuterxy <cuterxy@163.com>
2015-03-06 15:43:17 +0800

    improve the config locales files structure

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module RailsDevisePundit
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: false,
+        controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
@@ -32,7 +32,7 @@ module RailsDevisePundit
     config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'zh-CN'
   end
 end
