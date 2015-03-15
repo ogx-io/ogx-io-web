023d41ce642905b35065cbe81983cb5fbc0264dd
cuterxy <cuterxy@163.com>
2015-03-15 16:47:35 +0800

    improve the feature and step definitions of user signing in and out

--- features/desktop/user_signs_in_out.feature
+++ features/desktop/user_signs_in_out.feature
@@ -2,11 +2,15 @@ Feature: user authentication
 
   Scenario: user signs in
     Given I am a user with username "test_user" and password "password"
-    When I sign in
-    Then I should sign in successfully
+    When I sign in manually as "test_user" with password "password"
+    Then I would sign in
 
   Scenario: user signs out
-    Given I am a user with username "test_user" and password "password"
-    And I sign in
+    Given I have already signed in as "test_user" with password "password"
     And I click the "Sign Out" button in the header
-    Then I should sign out successfully
+    Then I would sign out
+
+  Scenario: fail to sign in by a wrong password
+    Given I am a user with username "test_user" and password "password"
+    When I sign in manually as "test_user" with password "wrong_password"
+    Then I would not sign in
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,11 +1,11 @@
-When /^I sign in$/ do
+When /^I sign in manually as "(.*?)" with password "(.*?)"$/ do |name, password|
   visit new_user_session_url
-  fill_in 'user_name', with: @me.name
-  fill_in 'user_password', with: @me.password
+  fill_in 'user_name', with: name
+  fill_in 'user_password', with: password
   click_button I18n.t('action.sign_in')
 end
 
-Then /^I should sign in successfully$/ do
+Then /^I would sign in$/ do
   expect(page).to have_content @me.nick
 end
 
@@ -13,6 +13,16 @@ And /^I click the "Sign Out" button in the header$/ do
   click_link I18n.t('action.sign_out')
 end
 
-Then /^I should sign out successfully$/ do
+Then /^I would sign out$/ do
   expect(page).not_to have_content @me.nick
+end
+
+Then /^I would not sign in/ do
+  expect(page).not_to have_content @me.nick
+end
+
+Given /^I have already signed in as "(.*?)" with password "(.*?)"$/ do |name, password|
+  step %(I am a user with username "#{name}" and password "#{password}")
+  step %(I sign in manually as "#{name}" with password "#{password}")
+  step %(I would sign in)
 end
