cfc261e5d03bfa7d353f61dcdae10b37c004563c
cuterxy <cuterxy@163.com>
2015-03-18 15:04:03 +0800

    improve the topic click count feature definition

--- features/desktop/topic_click_count.feature
+++ features/desktop/topic_click_count.feature
@@ -5,3 +5,5 @@ Feature: Record and show the click count of a topic
     Then the click count of the topic should be 1
     And visit the topic
     Then the click count of the topic should be 2
+    And visit the topic on the page 2
+    Then the click count of the topic should be 2
--- features/step_definitions/topic_steps.rb
+++ features/step_definitions/topic_steps.rb
@@ -10,3 +10,7 @@ end
 Then(/^the click count of the topic should be (\d+)$/) do |count|
   expect(find('.topic-click-count')).to have_content(I18n.t('topics.click_count', count: count))
 end
+
+And(/^visit the topic on the page (\d+)$/) do |page|
+  visit topic_path(@topic, page: 2)
+end
