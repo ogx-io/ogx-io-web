1690e4b7a4a45034cb576b6efd5869021bdce8be
Zhang Kai Yu <yeannylam@gmail.com>
2015-04-20 12:08:11 +0800

    add some information about omniauth callback controller test

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -3,5 +3,21 @@ require "rails_helper"
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
   describe "handles github callback" do
     pending()
+    # github_oauth_data = {
+    #   "credentials" => { "token" => "abcde" },
+    #   "extra" => { "raw_info" => {
+    #      "login" => "username",
+    #      "email" => "email@example.com",
+    #      "name" => "My Name",
+    #      "blog" => "blog.example.com",
+    #      "location" => "Beijing, China",
+    #      "avatar_url" => "http://www.baidu.com",
+    #      "id" => "12345"
+    # } } }
+    # @request.env["devise.mapping"] = Devise.mappings[:user]
+    # @request.env['omniauth.auth'] = github_oauth_data
+    # get :github
+    # expect(response).to be_success
+
   end
 end
