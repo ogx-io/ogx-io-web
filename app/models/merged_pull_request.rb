ab57f43f26c98379b79efe5c5c3462a7e673c1a7
cuterxy <cuterxy@163.com>
2015-05-12 23:17:55 +0800

    add the merged pull request list

---
+++ app/models/merged_pull_request.rb
@@ -0,0 +1,28 @@
+class MergedPullRequest
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :pr_type, type: String
+  field :remote_id, type: String
+  field :remote_user_id, type: String
+  field :raw_info, type: String
+  field :repos, type: String
+  field :remote_created_at, type: Time
+  field :merged_at, type: Time
+  field :title, type: String
+  field :link, type: String
+  field :remote_user_name
+  field :remote_user_link
+  field :remote_user_avatar
+
+  def local_user
+    user = nil
+    if pr_type == 'GitHub'
+      user = User.find_by(github_id: remote_user_id)
+    end
+    return user
+  rescue
+    return nil
+  end
+
+end
---
+++ spec/factories/merged_pull_requests.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :merged_pull_request do
+    type ""
+    remote_id "MyString"
+    remote_user_id "MyString"
+    raw_info "MyString"
+  end
+
+end
