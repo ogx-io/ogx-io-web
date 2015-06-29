a909ab0d798b592250e61f7c4f63ea6e44c75d84
cuterxy <cuterxy@163.com>
2015-06-29 22:31:33 +0800

    run rake tasks with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,11 +46,3 @@ namespace :deploy do
   end
 
 end
-
-namespace :rake do
-  desc "Run a task on a remote server."
-  # run like: cap staging rake:invoke task=a_certain_task
-  task :invoke do
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")
-  end
-end
